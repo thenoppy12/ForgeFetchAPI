f5b8d2089e3a6b1159869f14519fc27b68e6be79
MrNavaStar <Mr.NavaStar@gmail.com>
2023-01-21 23:22:39 -0700

    Fix urls for older mc versions

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 import json
 import xml.etree.ElementTree as Tree
 from requests import get
-from packaging.version import Version
+from packaging.version import Version, InvalidVersion
 
 
 def parse(xml_url: str, promotions_url: str):
@@ -11,10 +11,23 @@ def parse(xml_url: str, promotions_url: str):
     forge_versions = {}
     for game_version in xml.findall("./versioning/versions/version"):
         mc_version, forge_version = game_version.text.split("-", 1)
+        base_url = f"https://maven.minecraftforge.net/net/minecraftforge/forge/{mc_version}-{forge_version}/forge-{mc_version}-{forge_version}"
+        client_url = ""
+        server_url = ""
 
-        installer_url = f"https://maven.minecraftforge.net/net/minecraftforge/forge/{mc_version}-{forge_version}/forge-{mc_version}-{forge_version}-installer.jar"
+        try:
+            clean_version = Version(forge_version.split("-")[0])
 
-        i = f"https://maven.minecraftforge.net/net/minecraftforge/forge/{mc_version}-{forge_version}/forge-{mc_version}-{forge_version}-universal.zip"
+            if Version("7.7.2.682") >= clean_version >= Version("4.0.0.183"):
+                installer_url = f"{base_url}-universal.zip"
+            elif clean_version <= Version("4.0.0.182"):
+                client_url = f"{base_url}-client.zip"
+                server_url = f"{base_url}-server.zip"
+            else:
+                installer_url = f"{base_url}-installer.jar"
+        except InvalidVersion:
+            print(f"Bad version formatting: {forge_version}, using default url, may be broken")
+            installer_url = f"{base_url}-installer.jar"
 
         version_type = "release"
         if mc_version + "-latest" in promotions and forge_version == promotions[mc_version + "-latest"]:
@@ -25,7 +38,10 @@ def parse(xml_url: str, promotions_url: str):
         if mc_version not in forge_versions:
             forge_versions[mc_version] = []
 
-        forge_versions[mc_version].append({"id": forge_version, "type": version_type, "url": installer_url})
+        if client_url == "" and server_url == "":
+            forge_versions[mc_version].append({"id": forge_version, "type": version_type, "url": installer_url})
+        else:
+            forge_versions[mc_version].append({"id": forge_version, "type": version_type, "client_url": client_url, "server_url": server_url})
 
     ordered_keys = sorted(forge_versions, key=Version, reverse=True)
     return {key: forge_versions[key] for key in ordered_keys}
@@ -36,8 +52,5 @@ if __name__ == '__main__':
     promotions_url = "https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json"
 
     data = parse(xml_url, promotions_url)
-
-    print(data.keys())
-
     with open("web/forge-versions.json", "w") as file:
         file.write(json.dumps(data, indent=4))
